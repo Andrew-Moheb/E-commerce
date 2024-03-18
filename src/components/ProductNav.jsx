93649cb6136eee5d4c7e2e8d6da870bb2d06a99d
Andrew-Moheb <andrewmoheb500@gmail.com>
2024-03-18 17:02:03 +0200

    new test

--- src/components/ProductNav.jsx
+++ src/components/ProductNav.jsx
@@ -44,6 +44,9 @@ function ProductNav({ filter, setFilter, products }) {
         >
           Electronic
         </button>
+        <button className="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300">
+          test
+        </button>
       </div>
     </div>
   );
